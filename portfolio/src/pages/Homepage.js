11c1e5643c99b6bc9c8705e5979039622e70eca0
divyasri-kandavalli <divyasrikandavalli12@gmail.com>
2025-09-16 19:02:33 +0530

    first push

---
+++ portfolio/src/pages/Homepage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Homebanner from '../components/home-components/Homebanner'
+export default function Homepage() {
+  return (
+    <div>
+     <Homebanner/>
+    </div>
+  )
+}
